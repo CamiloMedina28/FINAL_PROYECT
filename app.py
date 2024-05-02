fc839cac4f46230d05e8a5b628c556990dccfb08
JaredMijailRE <jramirezes@unal.edu.co>
2024-05-02 00:51:57 -0500

    Se trabajo en el front-end y en el cambio de pestaña
    cambios pagina index, se aadieron la pagina ajustes e ingreso, el cuadro para ingresar estan los elementos falta ponerlo lindo, lo del modo noche toca estudiarlo

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
