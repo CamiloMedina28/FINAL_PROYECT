9ea19d91404b3c80f409cbe54226aaae414abff4
Camilo Medina <medinacamilo2829@gmail.com>
2024-06-16 20:21:29 -0500

    Cambios en las funciones de visualización

--- static/js/informacion_personal.js
+++ static/js/informacion_personal.js
@@ -39,3 +39,18 @@ function add_info_form(){
     document.getElementById('tabla_info').style.display = 'none';
     document.getElementById('formulario_añadir').style.display = 'flex';
 }
+
+function add_informacion_contacto(){
+    var documento = document.getElementById('documento').value;
+    var telefono_principal = document.getElementById('telefono_principal').value;
+    var correo = document.getElementById('correo').value;
+    var telefono_adicional = document.getElementById('telefono_adicional').value;
+    var correo_adicional = document.getElementById('correo_adicional').value;
+    fetch(`/agregar-info-contacto?documento=${documento}&telefono-principal=${telefono_principal}&correo=${correo}&telefono-adicional=${telefono_adicional}&correo-adicional=${correo_adicional}`)
+    .then((response) => response.text())
+    .then((data) => {
+        // window.location.href = "http://127.0.0.1:5010/bibliotecario/libros";
+        alert(data);
+    });
+
+}
